b2fe2118ffb87206b14961ba4e909c9a4f677f85
RogueArt <altoraries@gmail.com>
2021-06-25 22:57:45 -0700

    Added tests to the file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,55 @@
-use num::Float;
+use num::Integer;
+use std::{thread, time::Duration};
 
-fn add<T: Float>(x: T, y: T) -> T { x + y }
-fn sub<T: Float>(x: T, y: T) -> T { x - y }
-fn mult<T: Float>(x: T, y: T) -> T { x * y }
-fn div<T: Float>(x: T, y: T) -> T { x / y }
+// Some basic functions
+fn add<T: Integer>(x: T, y: T) -> T { x + y }
+fn sub<T: Integer>(x: T, y: T) -> T { x - y }
+fn mult<T: Integer>(x: T, y: T) -> T { x * y }
+fn div<T: Integer>(x: T, y: T) -> T { x / y }
 
-fn main() {
-    println!("{}", add(4.0, 2.0));
+// Simulate a heavy computation
+const HALF_SECOND: Duration = Duration::from_millis(500);
+fn heavy_computation<T: Integer>(x: T) -> T {
+    thread::sleep(HALF_SECOND);
+    x
+}
+
+fn main() {}
+
+// These tests kind of suck, but oh well
+#[cfg(test)]
+mod meh_tests {
+    use super::*;
+    #[test]
+    fn check_add() {
+        assert_eq!(add(2, 6), 8);
+        assert_eq!(add(2, 0), 2);
+        assert_eq!(add(2, -2), 0);
+    }
+
+    #[test]
+    fn check_sub() {
+        assert_eq!(sub(2, 6), -4);
+        assert_eq!(sub(2, 0), 2);
+        assert_eq!(sub(2, -2), 4);
+    }
+
+    #[test]
+    fn check_mult() {
+        assert_eq!(mult(2, 6), 12);
+        assert_eq!(mult(2, 0), 0);
+        assert_eq!(mult(2, -2), -4);
+    }
+
+    #[test]
+    fn check_div() {
+        assert_eq!(div(2, 6), 0);
+        assert_eq!(div(2, 1), 2);
+        assert_eq!(div(2, -2), -1);
+    }
+
+    // #[test]
+    // fn heavy_computation() {
+    //     assert_eq!(heavy_computation(3), 3);
+    // }
 }
