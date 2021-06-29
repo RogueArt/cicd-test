168d4c6eae88153734a6d1704f207e0fe7a86a45
RogueArt <altoraries@gmail.com>
2021-06-28 22:16:56 -0700

    Added replayability

--- src/main.rs
+++ src/main.rs
@@ -3,49 +3,64 @@ mod math;
 // For generating our guess number
 use rand::{thread_rng, Rng};
 
+use std::ascii::AsciiExt;
 // For getting input from the console
 use std::io::{self, stdin, Write};
 use std::process::{self, Command, Stdio};
 
+const MIN: u8 = 0;
+const MAX: u8 = 50;
+
 fn main() {
-    // Generate the target number ahead of time 
-    let mut rng = thread_rng();
-    let target_num: u8 = rng.gen_range(0..50);
-
-    // Begin the guessing game
-    println!("Let's play a guessing game!");
-    println!("Guess a number between {:?}:", 0..50);
     loop {
-        print!("Guess: ");
-        io::stdout().flush().expect("Something went wrong flushing console output.");
+        // Generate the target number ahead of time 
+        let mut rng = thread_rng();
+        let target_num: u8 = rng.gen_range(MIN..MAX);
 
-        // Get the guess
-        let mut input = String::new();
-        stdin().read_line(&mut input).expect("You entered an invalid string.");
+        // Begin the guessing game
+        println!("Let's play a guessing game!");
+        println!("Guess a number between {} and {}.", MIN, MAX);
+        loop {
+            print!("Guess: ");
+            io::stdout().flush().expect("Something went wrong flushing console output.");
 
-        // If starts with q, assume that they're trying to leave the game
-        if input.trim().starts_with('q') { 
-            println!("Exiting the game...");
-            process::exit(0);
+            // Get the guess
+            let mut input = String::new();
+            stdin().read_line(&mut input).expect("You entered an invalid input.");
+
+            // If starts with q, assume that they're trying to leave the game
+            if input.trim().starts_with('q') { 
+                println!("Exiting the game...");
+                process::exit(0);
+            }
+
+            // Convert it to an int, check if it's the same as our target
+            match input.trim().parse::<u8>() {
+                Ok(num) => {
+                    match num {
+                        n if n < target_num => println!("{} is too small", n),
+                        n if n > target_num => println!("{} is too big", n),
+                        _ => break
+                    }
+                },
+                // If error, ask them to type it in again
+                Err(e) => {
+                    println!("Try again with a number from {} to {}", MIN, MAX);
+                    continue;
+                }
+            };    
         }
 
-        // Convert it to an int, check if it's the same as our target
-        match input.trim().parse::<u8>() {
-            Ok(num) => {
-                match num {
-                    n if n < target_num => println!("{} is too small", n),
-                    n if n > target_num => println!("{} is too big", n),
-                    _ => break
-                }
-            },
-            // If error, ask them to type it in again
-            Err(e) => {
-                println!("Try again with a number from {:?}", 0..50);
-                continue;
-            }
-        };    
-    }
+        // If broke out of inner loop, means you win the game
+        println!("Congratulations! You win! The number was {}", target_num);
 
-    // If broke out of statement, means you win the game
-    println!("Congratulations! You win! The number was {}", target_num);
+        // Ask if player wants to play again, check their response
+        println!("Would you like to play again? (Y/N)");
+        let mut response = String::new();
+        stdin().read_line(&mut response).expect("You entered an invalid input.");
+
+        // Repeat the game if starts with y, otherwise assume break
+        if response.trim().to_ascii_lowercase().starts_with('y') { continue; }
+        else { break; }
+    }
 }
